f4adb33e50f9c8936f98dfadc659777bf5a2c56b
Luinix <luizmarcelolh@gmail.com>
2025-02-09 02:34:20 -0300

    update fonte final

--- U4C6O12T.c
+++ U4C6O12T.c
@@ -8,6 +8,7 @@
 #include "inc/ssd1306.h"
 #include "pico/time.h"
 #include "inc/font.h"
+#include <locale.h>
 
 #define I2C_PORT i2c1
 #define I2C_SDA 14
@@ -23,11 +24,10 @@ uint32_t tempo_botao_pressionado = 0; // Tempo de pressionamento do botão em ms
 #define LED_BLUE 11
 #define LED_GREEN 12
 
-bool updateDisplay = true;
-
 bool ledBlue = false;
 bool ledGreen = false;
 
+ssd1306_t ssd;
 char charDigitado = 'A';
 
 void actionMatriz(int key, PIO pio, uint sm)
@@ -146,47 +146,81 @@ void actionMatriz(int key, PIO pio, uint sm)
   }
 }
 
+char *on_off(bool on)
+{
+  if (on)
+    return "^";
+  else
+    return "`";
+}
+
+void updateDisplay()
+{
+  printf("Atualizando display\n");
+  ssd1306_fill(&ssd, false); // Limpa o display
+  char buffer[15];
+  char buffer2[14];
+  sprintf(buffer, "Led: G:%s  B:%s", on_off(ledGreen), on_off(ledBlue));
+  if (!charDigitado)
+    charDigitado = ' ';
+  sprintf(buffer2, "[%c] - U4C6O12T", charDigitado);
+  ssd1306_draw_string(&ssd, buffer2, 8, 10);         // Desenha uma string
+  ssd1306_draw_string(&ssd, buffer, 10, 25);         // Desenha uma string
+  ssd1306_draw_string(&ssd, "Aluno Luinix", 15, 48); // Desenha uma string
+  ssd1306_send_data(&ssd);                           // Atualiza o display
+}
+
 void button_irq_handler(uint gpio, uint32_t events)
 {
   uint32_t current_time = to_ms_since_boot(get_absolute_time());
   if (current_time - tempo_botao_pressionado < DEBOUNCE_TIME)
     return;
 
-  printf("Botão pressionado no pino %d\n", gpio);
   if (gpio == BUTTON_A)
   {
     printf("Botão A\n");
-    gpio_put(LED_BLUE, ledBlue = !ledBlue);
-    updateDisplay = true;
+    gpio_put(LED_GREEN, ledGreen = !ledGreen);
   }
   else
   {
-    gpio_put(LED_GREEN, ledGreen = !ledGreen);
+    gpio_put(LED_BLUE, ledBlue = !ledBlue);
     printf("Botão B\n");
-    updateDisplay = true;
+  }
+  updateDisplay();
+  tempo_botao_pressionado = current_time;
+}
+
+void updateMatriz(char charDigitado, PIO pio, uint sm)
+{
+  if (charDigitado >= '0' && charDigitado <= '9')
+  {
+    actionMatriz(charDigitado, pio, sm);
+  }
+  else
+  {
+    actionMatriz(' ', pio, sm);
   }
 }
 
 int main()
 {
+  setlocale(LC_ALL, "Portuguese_Brazil");
+
   stdio_init_all();
-  // I2C Initialisation. Using it at 400Khz.
   i2c_init(I2C_PORT, 400 * 1000);
 
-  //============== INICIALIZAÇÃO DISPLAY
-  gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);                    // Set the GPIO pin function to I2C
-  gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);                    // Set the GPIO pin function to I2C
-  gpio_pull_up(I2C_SDA);                                        // Pull up the data line
-  gpio_pull_up(I2C_SCL);                                        // Pull up the clock line
-  ssd1306_t ssd;                                                // Inicializa a estrutura do display
-  ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); // Inicializa o display
-  ssd1306_config(&ssd);                                         // Configura o display
-  ssd1306_send_data(&ssd);                                      // Envia os dados para o display
-  // Limpa o display. O display inicia com todos os pixels apagados.
+  gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+  gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+  gpio_pull_up(I2C_SDA);
+  gpio_pull_up(I2C_SCL);
+
+  ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT);
+  ssd1306_config(&ssd);
+  ssd1306_send_data(&ssd);
+
   ssd1306_fill(&ssd, false);
   ssd1306_send_data(&ssd);
 
-  //============== INICIALIZAÇÃO BOTÕES
   gpio_init(BUTTON_A);
   gpio_init(BUTTON_B);
   gpio_set_dir(BUTTON_A, GPIO_IN);
@@ -197,7 +231,6 @@ int main()
   gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL, true, &button_irq_handler);
   gpio_set_irq_enabled(BUTTON_B, GPIO_IRQ_EDGE_FALL, true);
 
-  //============== INICIALIZAÇÃO LEDS
   gpio_init(LED_BLUE);
   gpio_init(LED_GREEN);
   gpio_set_dir(LED_BLUE, GPIO_OUT);
@@ -205,37 +238,19 @@ int main()
   gpio_put(LED_BLUE, 0);
   gpio_put(LED_GREEN, 0);
 
-  //============== INICIALIZAÇÃO MATRIZ
   PIO pio = pio0;
   uint sm = configurar_matriz(pio);
   actionMatriz(' ', pio, sm);
 
   printf("Bem vindo a Atividade U4C6O12T!\n");
+  updateDisplay();
+
   while (true)
   {
-    if (updateDisplay || scanf(" %c", &charDigitado))
+    if (scanf(" %c", &charDigitado))
     {
-      if (charDigitado >= '0' && charDigitado <= '9')
-      {
-        actionMatriz(charDigitado, pio, sm);
-      }
-      else
-      {
-        actionMatriz(' ', pio, sm);
-      }
-      printf("Atualizando display\n");
-      updateDisplay = false;
-      ssd1306_fill(&ssd, false); // Limpa o display
-      char buffer[15];
-      char buffer2[14];
-      sprintf(buffer, "Led: G:%d, B:%d", ledGreen, ledBlue);
-      if (!charDigitado)
-        charDigitado = ' ';
-      sprintf(buffer2, "[%c] - U4C6O12T", charDigitado);
-      ssd1306_draw_string(&ssd, buffer2, 8, 10);         // Desenha uma string
-      ssd1306_draw_string(&ssd, buffer, 10, 25);         // Desenha uma string
-      ssd1306_draw_string(&ssd, "Aluno Luinix", 15, 48); // Desenha uma string
-      ssd1306_send_data(&ssd);                           // Atualiza o display
+      updateMatriz(charDigitado, pio, sm);
+      updateDisplay();
     }
     sleep_ms(200);
   }
